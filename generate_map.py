e41ec9dafc0fa9a7ed55e27e23804bfa50eb3674
Arthur Juliani <awjuliani@gmail.com>
2025-12-02 19:57:39 -0800

    Use MDS to avoid bias

--- generate_map.py
+++ generate_map.py
@@ -21,13 +21,13 @@ def main():
         "--algo",
         type=str,
         choices=["mds", "tsne"],
-        default="tsne",
+        default="mds",
         help="Dimensionality reduction algorithm to use (mds or tsne)",
     )
     parser.add_argument(
         "--perplexity",
         type=float,
-        default=10.0,
+        default=9.0,
         help="Perplexity for t-SNE (recommended < n_samples)",
     )
     parser.add_argument(
